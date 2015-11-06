33200c7ade5e0b63c60675e85f7f15ae5fa20c32
jeffoxenberg <joxenberg@gmail.com>
2015-11-06 17:00:18 -0500

    clean up

--- roles/performance/files/tuned.sh
+++ roles/performance/files/tuned.sh
@@ -3,15 +3,7 @@ swapoff -a
 for disk in /sys/block/sd?
 do
 echo 1 > $disk/queue/nomerges
-done
-
-for disk in /sys/block/sd?
-do
 echo 8 > $disk/queue/read_ahead_kb
-done
-
-for disk in /sys/block/sd?
-do
 echo deadline > $disk/queue/scheduler
 done
 
